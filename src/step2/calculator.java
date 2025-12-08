da8e69e892e8188800206a7eaa2c99a08d993bb0
MinWoo1995 <wjsalsdn120@naver.com>
2025-12-08 13:13:56 +0900

    feat: Step 2 완료. 캡슐화 적ì연산이 완료된 결과값 배열 저장 구현

--- src/step2/calculator.java
+++ src/step2/calculator.java
@@ -1,8 +1,11 @@
 package step2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class calculator {
     //속성
-    private int calculatorResult = 0;//연산 결과값 클래스에 캡슐화를 통한 저장
+    private List<Integer> calculatorResult = new ArrayList();//연산 결과값 클래스에 캡슐화를 통한 저장
     int result = 0;//연산 결과값 저장
 
     //생성자
@@ -47,10 +50,10 @@ public class calculator {
         return result;
     }
 
-    public int getResult() {
+    public List<Integer> getResult() {
         return this.calculatorResult;
     }
-    public void setResult(int result) {
+    public void setResult(List<Integer> result) {
         this.calculatorResult = result;
     }
 
